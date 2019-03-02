4d8e7669df97bb184febae2679ac3a96febe7b4e
KrzysztofWiech <bystrow7@gmail.com>
2019-03-02 15:55:02 +0100

    Initial Commit

---
+++ src/main/java/com/example/CalculatorBmi/BmiCalculator.java
@@ -0,0 +1,26 @@
+package com.example.CalculatorBmi;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+public class BmiCalculator {
+
+
+    private double weight;
+
+    private double height;
+
+    public double getBmiIndex(double height, double weight) {
+        height /= 100;
+
+        double bmiIndex = weight / (height * height);
+        return bmiIndex;
+
+
+    }
+
+
+}
